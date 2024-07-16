fef0002aff9c7607c15d91e76770c0d98f5fc5c5
Nayan-Bhisara <bhisaranayan3196@gmail.com>
2024-07-16 18:35:00 +0530

    Pr-12

---
+++ Pr-12/src/firebasedb.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFOPo96GP-Xil1lq9QHsRP1qklbb0dKVE",
+  authDomain: "pr-12-b2656.firebaseapp.com",
+  projectId: "pr-12-b2656",
+  storageBucket: "pr-12-b2656.appspot.com",
+  messagingSenderId: "974627745038",
+  appId: "1:974627745038:web:5bc32b0f5feea958413b5f",
+  measurementId: "G-2BC068RSX9"
+};
+
+export const app = initializeApp(firebaseConfig);
